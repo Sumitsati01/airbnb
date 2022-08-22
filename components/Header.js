aab97de760eebe0eb5159cbc80d32d5d162c36c0
Sumit Sati <sumit.sati@ebizondigital.com>
2022-08-22 17:52:17 +0530

    airbnb header left&middle

---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Image from 'next/image' 
+import {SearchIcon} from '@heroicons/react/solid'
+
+
+
+
+export default function Header() {
+  return (
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5
+    md:px-10
+    '>
+     {/* Left */}
+     <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+       <Image src='https://links.papareact.com/qd3' layout='fill' 
+       objectFit='contain' objectPosition='left'
+       />
+     </div>
+
+     {/* Middle */}
+     <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+         <input type='text' placeholder='Search here' className=' flex-grow pl-5 bg-transparent outline-none'/>
+         <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer'/>
+     </div>
+
+     {/* Right */}
+     <div>
+
+     </div>
+    </header>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 
 import Head from 'next/head'
+import Header from '../components/Header'
 
 
 const Home= () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>AirBnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Let's Build AirBnb</h1>
+     <Header />
+      {/* banner */}
      
     </div>
   )
