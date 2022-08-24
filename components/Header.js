5bcf0522b78fa5095667bb0f2bb29f4495f0894b
Sumit Sati <sumit.sati@ebizondigital.com>
2022-08-24 13:33:50 +0530

    calendar on search

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import Image from 'next/image' 
 import {SearchIcon ,GlobeAltIcon,UserCircleIcon,UserIcon,MenuIcon} from '@heroicons/react/solid'
-
+import { useState, useEffect } from 'react'
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 
 
 
 export default function Header() {
+  const [searchInput,setSearchInput]=useState("");
+  const [startDate,setStartDate]=useState(new Date());
+  const [endDate,setEndDate]=useState(new Date())
+
+  const selectRange={
+    key:'Selection',
+    startDate:startDate,
+    endDate:endDate
+  }
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5
     md:px-10
@@ -20,7 +32,8 @@ export default function Header() {
 
      {/* Middle */}
      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
-         <input type='text' placeholder='Search here' className=' flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600'/>
+         <input value={searchInput} onChange={(e)=>setSearchInput(e.target.value)}
+         type='text' placeholder='Search here' className=' flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600'/>
          <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'/>
      </div>
 
@@ -33,6 +46,17 @@ export default function Header() {
          <UserCircleIcon className='h-6'/>
        </div>
      </div>
+     {searchInput && (
+      <div className=''>
+        <DateRangePicker 
+        ranges={[selectRange]}
+        minDate={new Date()}
+        rangeColors={['#FD5B61']}
+        />
+        </div>
+     )
+
+     }
     </header>
   )
 }
