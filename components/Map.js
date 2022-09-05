3e41fbc9cc7e6055048a2b06d312b468af312ebc
Sumit Sati <sumit.sati@ebizondigital.com>
2022-09-05 16:54:11 +0530

    mapbox changes

--- components/Map.js
+++ components/Map.js
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import ReactMapGl,{Marker,Popup} from'react-map-gl'
 import getCenter from 'geolib/es/getCenter';
+import {LocationMarkerIcon} from '@heroicons/react/solid'
+
+// import 'mapbox-gl/dist/mapbox-gl.css';
+
+
 
 
 function Map({searchResults}) {
     const [selectedLocation,setSelectedLocation]=useState({});
+    
    //transformation of object
    const coordinates=searchResults.map(result=>({
      latitude:result.lat,
@@ -12,28 +18,46 @@ function Map({searchResults}) {
    }))
    const center = getCenter(coordinates);
    const [viewPort,setViewPort]=useState({
-    width:'100%',
-    height:'100%',
-    latitude:center.latitude,
+    width:'100vw',
+    height:'100vh',
     longitude:center.longitude,
-    zoom:11
+    latitude:center.latitude,
+    zoom: 11,
     });
    return (
     <ReactMapGl
-    mapStyle="mapbox://styles/sumit-sati/cl7k8prn9006614qgsilc495z"
+    initialViewState={viewPort}
+    mapStyle="mapbox://styles/sumit-sati/cl7odz1t1001v14qut2rc4t4u"
     mapboxAccessToken={process.env.mapbox_key}
-    {...viewPort}
-    onViewPortChange={(nextViewPort)=>setViewPort(nextViewPort)}
+    onViewportChange={(nextViewport)=>setViewPort(nextViewport)}
     >
-        {searchResults.map(result=>(
+    {searchResults.map(result=>(
             <div key={result.long}>
-             <Marker
+             <Marker 
              longitude={result.long}
              latitude={result.lat}
              offsetLeft={-20}
-             offsetTop={10}
+             offsetTop={-10}
              >
-                <p onClick={()=>setSelectedLocation(result)}><img height='100%' width='100%' src='../mark.jpeg'/></p></Marker>
+                <p
+                aria-label='push-pin'
+                className='animate-bounce cursor-pointer text-2xl'> 
+                <LocationMarkerIcon className="h-6 w-6 text-red-500"  onClick={()=>setSelectedLocation(result)}/>
+                </p>
+                </Marker>
+                {/* Popup shows on click of marker */}
+                {selectedLocation.long===result.long ?(
+                 <Popup 
+                 onClose={()=>setSelectedLocation({})}
+                 closeOnClick={true}
+                 latitude={result.lat}
+                 longitude={result.long}
+                 >{result.title}
+                 </Popup>
+                ):(
+                  false
+                )
+                }
             </div>
         ))}
     </ReactMapGl>
--- pages/search.js
+++ pages/search.js
@@ -46,7 +46,7 @@ function Search({searchResults}) {
             ))}
             </div>
         </section>
-        <section className='hidden xl:inline-flex xl:min-width-[600px]'>
+        <section className='hidden xl:inline-flex xl:min-w-[600px]'>
           <Map searchResults={searchResults}/>
         </section>
       </main>
